bb26c6a1f3217d216e67780a39cf422290c3ea77
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-11-16 11:53:40 +0100

    Creando Events

---
+++ upgrade-events.js
@@ -0,0 +1,27 @@
+// --------------------------------1.1------------------------------
+
+// const newBut = document.createElement("button")
+// const newButText = document.createTextNode("Pulsame")
+// newBut.id = "btnToClick"
+// newBut.onclick = function onclick() {
+//     console.log("acabas de hacer click")
+// }
+// newBut.appendChild(newButText)
+// document.body.appendChild(newBut)
+
+// --------------------------------1.2------------------------------
+// const input = document.getElementById("focuss")
+// input.addEventListener(focus, updateValue)
+
+// function updateValue(e) {
+//     let log = e.srcElement.value;
+//     console.log(log)
+// }
+// --------------------------------1.3------------------------------
+// const input = document.getElementById("valuee")
+// input.addEventListener("input", updateValue)
+
+// function updateValue(e) {
+//     let log = e.srcElement.value;
+//     console.log(log)
+// }
